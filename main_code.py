c9c083c4e3ee6cd5fe9ce582b1021ae6a0f36d1c
Gustavo Freitas <gprofi98@gmail.com>
2024-09-03 22:15:51 -0300

    We have result, but it is not good enough

--- main_code.py
+++ main_code.py
@@ -1,6 +1,8 @@
 import fastapi
 import torch
 import torch.nn as nn
+import torch.nn.functional as F
+from torchvision import transforms
 import cv2 as cv
 import os
 import numpy as np
@@ -38,14 +40,23 @@ import matplotlib.pyplot as plt
 class TakeImage:
 
     def __init__(self):
-        print('Here we need take a picture and process the image to appy the model')
+        # print('Here we need take a picture and process the image to appy the model')
 
-        # self.image = self.take_photo()
-        # self.crop_image(self.image)
-        self.find_weights()
+        self.image = self.take_photo_with_target()
+        final_image_ = self.crop_image(self.image)
 
-    def take_photo(self) -> np.ndarray:
-        print('Opening the webcam')
+        self.pred_, self.prob = self.apply_model(final_image_)
+
+        cv.putText(self.image, f'{int(self.pred_)} - {self.prob:.4f}', (50, 50),
+                   cv.FONT_HERSHEY_SIMPLEX, 1, (255, 0, 0), 1)
+        cv.imshow('Prediction', self.image)
+        if cv.waitKey(0) == ord('f'):
+            pass
+        print(f'The final result is: {self.pred_} with {self.prob:.4f}')
+
+    @staticmethod
+    def take_photo_with_target() -> np.ndarray:
+        # print('Opening the webcam')
         webcam_ = cv.VideoCapture(0)
         image_ = None
         if not webcam_.isOpened():
@@ -56,22 +67,26 @@ class TakeImage:
             if not ret:
                 print('The capture failed')
                 break
-            cv.imshow('WebCam Image', frame)
+            h_, w_, _ = frame.shape
+            frame_with_rect = cv.rectangle(frame, (int(0.2 * w_), int(0.2 * h_)), (int(0.8 * w_), int(0.8 * h_)),
+                                           (0, 255, 0), 2)
+            cv.imshow('WebCam Image', frame_with_rect)
             if cv.waitKey(1) == 32:
                 image_ = frame
-                print('Image captured')
+                # print('Image captured')
                 break
         webcam_.release()
         cv.destroyAllWindows()
         return image_
 
-    def crop_image(self, image: np.ndarray) -> np.ndarray:
+    @staticmethod
+    def crop_image(image: np.ndarray) -> np.ndarray:
         '''
         Here I thought just crop the image in the center, like QR code identify
         :param image: Webcam image capture
         :return: Image processed and resized
         '''
-        print(f'image shape is {image.shape}')
+        # print(f'image shape is {image.shape}')
         image_gray = cv.cvtColor(image, cv.COLOR_BGR2GRAY)
         image_invert = cv.bitwise_not(image_gray)
         image_gaussian_blur = cv.GaussianBlur(image_invert, (3, 3), 0)
@@ -81,24 +96,57 @@ class TakeImage:
         image_crop = np.copy(image_canny)
         height, width = image_crop.shape
         image_crop = image_crop[int(0.2 * height):int(0.8 * height), int(0.2 * width):int(0.8 * width)]
-        cv.imshow('Crop image', image_crop)
-        if cv.waitKey(0) == ord('q'):
-            cv.destroyAllWindows()
+        # cv.imshow('Crop image', image_crop)
+        # if cv.waitKey(0) == ord('q'):
+        #     cv.destroyAllWindows()
         return cv.resize(image_crop, (28, 28))
 
-    def find_weights(self) -> str:
+    @staticmethod
+    def find_weights() -> str:
         weight_file = None
         for _, _, files in os.walk(os.getcwd()):
             files_pt = [file for file in files if file.endswith('.pt')]
-            weight_file = files_pt[0]
+            if len(files_pt) != 0:
+                weight_file = files_pt[0]
+                return weight_file
         return weight_file
 
-    def apply_model(self) -> int:
+    def apply_model(self, image_data: np.ndarray) -> tuple:
 
-        cnn = CNN('cpu')
-        cnn_load = cnn.load_state_dict(str(self.find_weights()))
+        pt_file_ = r'C:\Users\Gustavo - Win\Documents\AI_projects\IA048_Machine_Learning\best_model_model_1541.pt'
 
-        return 0
+        cnn = CNN()
+
+        state_dict_ = torch.load(pt_file_, map_location='cpu', weights_only=True)
+
+        cnn.load_state_dict(state_dict_)
+
+        transform = transforms.Compose([
+            transforms.ToPILImage(),
+            transforms.ToTensor()
+        ])
+
+        data_tensor_ = transform(image_data)
+        data_batch_ = data_tensor_.unsqueeze(0)
+
+        cnn.eval()
+
+        with torch.no_grad():
+            out_ = cnn(data_batch_)
+
+        _, pred_ = torch.max(out_, 1)
+
+        # print(f'{pred_=}')
+
+        probs = torch.nn.functional.softmax(out_[0], dim=0)
+
+        # print(f'{probs=}')
+
+        pred_prob = probs[pred_[0]].item()
+
+        # print(f'{pred_prob=}')
+
+        return pred_.item(), pred_prob
 
 
 class UploadImage:
